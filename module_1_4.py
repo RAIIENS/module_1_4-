7764f2afab78d0399cfd3a278084989cf2dae8e6
Vladimir N <evaeva6202@gmail.com>
2024-10-20 15:57:16 +0300

    module_1_4 "Организация программ и методы строк"
    module_1_4 "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,18 @@
+# Создайте переменную my_string
+# и присвойте ей значение строки с произвольным текстом (функция input())
+# my_string = input('Введите:"Вадим и Ольга"')
+my_string = "Вадим и Ольга"
+print(my_string)
+# Вывести количество символов введённого текста
+print(len(my_string))
+# Выведите строку my_string в верхнем регистре.
+print(my_string.upper())
+# Выведите строку my_string в нижнем регистре.
+print(my_string.lower())
+# Измените строку my_string, удалив все пробелы.
+my_string = "Вадим и Ольга".replace(" ", "")
+print(my_string)
+# Выведите первый символ строки my_string.
+print(my_string[0])
+# Выведите последний символ строки my_string.
+print(my_string[-1])
